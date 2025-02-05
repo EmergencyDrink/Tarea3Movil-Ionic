4ed5d9d026d9f449b3596583320906dec3feecc3
EmergencyDrink <andy123cx@gmail.com>
2025-02-04 22:18:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tarea3Movil',
+  webDir: 'www'
+};
+
+export default config;
